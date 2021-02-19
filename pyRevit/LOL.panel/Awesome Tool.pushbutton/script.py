a3db38d50b51b655ff04f1e06af411db1c334a38
renatogcruz <renatogcruz@hotmail.com>
2021-02-19 17:51:44 -0300

    first script pyRevit \o/

---
+++ pyRevit/LOL.panel/Awesome Tool.pushbutton/script.py
@@ -0,0 +1,7 @@
+__title__ = "Awesome tool"
+
+__doc__ = "This tool creates a dialog window"
+
+from Autodesk.Revit.UI import TaskDialog
+
+TaskDialog.Show("Hello Renato", "Think Python, think pyRevit!")
